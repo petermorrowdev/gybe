a9c8aee4a679884d7601dbb4cd0a3170b88b97ed
Peter Morrow <petermorrowtech@gmail.com>
2024-08-18 16:21:32 -0700

    Starts developing k8s resource model for Deployment, Job, StatefulSet, ect..

--- gybe/k8s/types.py
+++ gybe/k8s/types.py
@@ -1,12 +1,14 @@
 """Type aliases for JSON serializable objects."""
 
-from dataclasses import dataclass
 from typing import Mapping, TypeAlias, Union
 
 JSONObj: TypeAlias = Union[Mapping[str, 'JSONObj'], list['JSONObj'], str, int, float, bool, None]
 JSONDict: TypeAlias = Mapping[str, Union['JSONObj', 'JSONDict']]
 
 
-@dataclass
 class K8sSpec:
     """Base model class for all kubernetes dataclasses."""
+
+
+class K8sResource(K8sSpec):
+    """Base model for kubernetes resources, like Deployment, Service and StatefulSet"""
