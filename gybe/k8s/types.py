6a620108e793dc97c367d19e0d445b9094382f47
Peter Morrow <petermorrowtech@gmail.com>
2024-08-24 10:01:39 -0700

    Decorates base classes with @dataclass.
    This is a concise way to tell mypy we excpect subclasses to also be
    dataclasses.

--- gybe/k8s/types.py
+++ gybe/k8s/types.py
@@ -1,14 +1,19 @@
 """Type aliases for JSON serializable objects."""
 
+from dataclasses import dataclass
 from typing import Mapping, TypeAlias, Union
 
 JSONObj: TypeAlias = Union[Mapping[str, 'JSONObj'], list['JSONObj'], str, int, float, bool, None]
 JSONDict: TypeAlias = Mapping[str, Union['JSONObj', 'JSONDict']]
 
 
+# decorating the base classes with @dataclass has no runtime effect but tells `mypy` to
+# expect subclasses to each be a dataclass
+@dataclass
 class K8sSpec:
     """Base model class for all kubernetes dataclasses."""
 
 
+@dataclass
 class K8sResource(K8sSpec):
     """Base model for kubernetes resources, like Deployment, Service and StatefulSet"""
