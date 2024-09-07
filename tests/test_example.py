233e6fffb1d1e49ebd3f75ad8e38d172a5fc0a10
Peter Morrow <petermorrowtech@gmail.com>
2024-09-07 12:39:09 -0700

    Includes test to replicate the issue

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,7 +6,7 @@ def create_standard_container(image: str, command: list[str]):
 
 
 @gybe.transpiler
-def two_pods(image: str, command: list[str], port: int = 8080) -> gybe.Manifest:
+def two_pods(image: str, command: list[str]) -> gybe.Manifest:
     pod_spec = gybe.k8s.PodSpec(
         containers=[create_standard_container(image=image, command=command)],
     )
@@ -62,11 +62,6 @@ spec:
     name: standard-server
 """
 
-INVALID_VALID_TWO_POD_YAML = """
-image: python:3
-command: false
-"""
-
 
 def test_two_pods_chart_transpiles_with_valid_yaml(run_cli):
     result = run_cli(two_pods, VALID_TWO_POD_YAML)
@@ -74,6 +69,31 @@ def test_two_pods_chart_transpiles_with_valid_yaml(run_cli):
     assert result.stdout.strip() == EXPECTED_TWO_POD_MANIFEST.strip()
 
 
-def test_two_pods_chart_fails_with_invalid_yaml(run_cli):
-    result = run_cli(two_pods, INVALID_VALID_TWO_POD_YAML)
+EXPECTED_EMPTY_YAML_VALIDATION_ERROR = """
+validation errors:
+- required field missing @ $.image
+- required field missing @ $.command
+"""
+
+
+def test_two_pods_chart_fails_with_empty_yaml(run_cli):
+    result = run_cli(two_pods, '')
     assert result.exit_code == -1
+    assert result.stdout.strip() == EXPECTED_EMPTY_YAML_VALIDATION_ERROR.strip()
+
+
+INVALID_TWO_POD_YAML = """
+image: python:3
+command: false
+"""
+
+EXPECTED_INVALID_TWO_POD_YAML = """
+validation errors:
+- invalid value for type, expected list @ $.command
+"""
+
+
+def test_two_pods_chart_fails_with_invalid_yaml(run_cli):
+    result = run_cli(two_pods, INVALID_TWO_POD_YAML)
+    assert result.exit_code == -1
+    assert result.stdout.strip() == EXPECTED_INVALID_TWO_POD_YAML.strip()
