9b4168b56288784b0d2ca20f1465c16574b17a53
Peter Morrow <petermorrowtech@gmail.com>
2025-02-24 20:25:16 -0800

    Remove no cov for codegen cli
    - Write tests for here later

--- gybe/codegen/cli.py
+++ gybe/codegen/cli.py
@@ -5,7 +5,7 @@ import argparse
 from gybe.codegen.k8s_modules import write_module
 
 
-def main():  # no cov
+def main():
     """Run gybe.k8s code generator cli"""
     parser = argparse.ArgumentParser()
     parser.add_argument('k8s_version_module')
